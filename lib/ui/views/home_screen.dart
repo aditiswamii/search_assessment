5d327a3add89aa7b174fd08f9515c15e403b841e
aditi swami <aditi.swami@neologicx.com>
2022-12-06 22:42:37 +0530

    update

--- lib/ui/views/home_screen.dart
+++ lib/ui/views/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:search_assessment/core/core.dart';
+import 'package:search_assessment/ui/widget/widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,19 +10,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      body: Stack(
-        children: [
-
-        ],
-      )
-      // This trailing comma makes auto-formatting nicer for build methods.
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+                height: 70,
+                width: MediaQuery.of(context).size.width,
+                color: AppTheme.appBlack,
+                child: SearchBarWidget(
+                  bodyMargin: MediaQuery.of(context).size.width * 0.07,
+                )),
+            Container(
+              margin: const EdgeInsets.only(top: 70),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/widget/search_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:search_assessment/core/core.dart';
+
+class SearchBarWidget extends StatelessWidget {
+ final double bodyMargin;
+   SearchBarWidget({super.key,required this.bodyMargin});
+   TextEditingController searchController = TextEditingController();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: bodyMargin, vertical: 10),
+      child: TextFormField(
+        controller: searchController,
+        textInputAction: TextInputAction.search,
+        textDirection: TextDirection.ltr,
+        keyboardType: TextInputType.text,
+        style: TextStyle(color: AppTheme.appBlack),
+        cursorColor: AppTheme.appBlack,
+        cursorHeight: 0.0,
+        cursorWidth: 0.0,
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+            fillColor: AppTheme.appWhite,
+            filled: true,
+            hintText: 'Search Some User',
+            hintStyle: TextStyle(color: AppTheme.appGrey),
+            prefixIcon: Icon(Icons.search,color: AppTheme.appGrey,size: 22,),
+            //prefixIconConstraints:BoxConstraints(minWidth: 23, maxHeight: 20),
+            suffixIcon: Icon(Icons.close,color: AppTheme.appGrey,size: 22,),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: AppTheme.appWhite)),
+            disabledBorder: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: AppTheme.appWhite)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: AppTheme.appWhite)),
+            focusedErrorBorder: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: AppTheme.appWhite)),
+            border: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: AppTheme.appWhite))),
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.singleLineFormatter
+        ],
+        onChanged: (value) {
+          debugPrint(value);
+          //  _cubit.getSearchProduct(text: searchController.text);
+        },
+        onFieldSubmitted: (String value) {
+          //
+          // _cubit.getSearchProduct(text: value);
+          // setState(() {
+          //   searchController.text=value;
+          // });
+        },
+      ),
+    );
+  }
+}
+
